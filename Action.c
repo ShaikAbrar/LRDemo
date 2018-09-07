fcb6191edf92705079eab1158fa716c3d2e60941
Software Admin <sw_admin@itriangle.in>
2018-09-07 11:33:07 +0530

    This is my first commit

---
+++ Action.c
@@ -0,0 +1,13 @@
+Action()
+{
+	return 0;
+}
+
+	web_custom_request("web_custom_request",
+		"URL=www.aquilatrack.zeliot.in",
+		"Method=GET",
+		"TargetFrame=",
+		"Resource=0",
+		"Referer=",
+		"Body=",
+		LAST);
